b277e0953189cb6226313a1e8f89aca094511014
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-06-12 10:54:12 -0400

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Button from "./Button.svelte";
+  import Logo from "./Logo.svelte";
+</script>
+
+<style>
+  main {
+    font-family: monospace;
+    text-align: center;
+  }
+</style>
+
+<main>
+  <Logo altText="Big Screw You" iurl="https://it.stonybrook.edu/sites/all/libraries/unity-lab/latest/images/logos/stony-brook-university-logo.jpg"></Logo>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button />
+</main>
---
+++ Logo.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let altText;
+  export let iurl;
+</script>
+
+<style>
+	logo {
+	  border: 2px;
+	  padding: 8px 12px;
+	  border-radius: 2px;
+	}
+</style>
+
+<logo>
+  <img alt={altText} src={iurl} />
+</logo>
